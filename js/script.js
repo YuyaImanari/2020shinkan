05d30b7921e3bf3caa8fd2bc86de81c4b09ef006
yuyaimanari <e.yuya129@gmail.com>
2020-02-16 00:50:34 +0900

    ヘッダーを変更

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 $(function () {
   $('#menu-btn').click(function () {
     var width = $(window).width();
+    var slideTime = 500;
     $(this).toggleClass('open')
     $('body').toggleClass('open');
     if ($(this).hasClass('open')) {
       $('body').css('overflow', 'hidden');
-      $('#menu').animate({'left': 0}, 300);
+      $('#SP').css('overflow', 'visible');
+      $('#menu').animate({ 'left': 0 }, slideTime);
     } else {
-      $('#menu').animate({'left': width}, 300);
       $('body').css('overflow', 'visible');
+      setTimeout(() => {
+        $('#SP').css('overflow', 'hidden');
+      }, slideTime);
+      $('#menu').animate({ 'left': width }, slideTime);
     }
   });
 });
