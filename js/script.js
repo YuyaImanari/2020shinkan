58faba081f8140df3fcc13ee21b42306697fd56a
yuyaimanari <e.yuya129@gmail.com>
2020-03-01 02:24:36 +0900

    一旦完成？

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 $(function () {
   $('#menu-btn').click(function () {
-    var width = $(window).width();
+    var win_wid = $(window).width();
+    var width = Math.min($(window).width(),414);
     var slideTime = 400;
     $(this).toggleClass('open')
     $('body').toggleClass('open');
     if ($(this).hasClass('open')) {
       $('body').css('overflow', 'hidden');
       $('#SP').css('overflow', 'visible');
-      $('#menu').animate({ 'left': 0 }, slideTime);
+      $('#menu').animate({ 'left': win_wid - width }, slideTime);
     } else {
       $('body').css('overflow', 'visible');
       setTimeout(() => {
         $('#SP').css('overflow', 'hidden');
       }, slideTime);
-      $('#menu').animate({ 'left': width }, slideTime);
+      $('#menu').animate({ 'left': win_wid }, slideTime);
     }
   });
-  $('.menu-samepage').click(function () {
-    var width = $(window).width();
-    $('#menu-btn').removeClass('open');
-    $('#menu').css('left', width);
-    $('body').removeClass('open');
-    $('body').css('overflow', 'visible');
-  });
+  // $('.menu-samepage').click(function () {
+  //   var width = $(window).width();
+  //   $('#menu-btn').removeClass('open');
+  //   $('#menu').css('left', width);
+  //   $('body').removeClass('open');
+  //   $('body').css('overflow', 'visible');
+  // });
 });
