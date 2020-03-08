4626b6fb909884f0790c4ae6041335f799f1acc3
yuyaimanari <e.yuya129@gmail.com>
2020-03-08 20:43:22 +0900

    局紹介修正

--- js/department.js
+++ js/department.js
@@ -8,8 +8,8 @@ $(function () {
   $('#ev-btn').click(function () {
     $('#modal-ev').fadeIn();
     $('body').css('overflow', 'hidden');
-    var wH = $(window).height();
-    var mH = $('.modal-wrapper').innerHeight();
+    var wH = $(window).height() * 0.948;
+    var mH = $('#modal-wrapper_ev').innerHeight();
     if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('iPod') > 0) {
       if (mH > wH) {
         bg.addEventListener('touchmove', prevent, {
@@ -40,8 +40,8 @@ $(function () {
   $('#sh-btn').click(function () {
     $('#modal-sh').fadeIn();
     $('body').css('overflow', 'hidden');
-    var wH = $(window).height();
-    var mH = $('.modal-wrapper').innerHeight();
+    var wH = $(window).height() * 0.948;
+    var mH = $('#modal-wrapper_sh').innerHeight();
     if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('iPod') > 0) {
       if (mH > wH) {
         bg.addEventListener('touchmove', prevent, {
@@ -72,8 +72,8 @@ $(function () {
   $('#pr-btn').click(function () {
     $('#modal-pr').fadeIn();
     $('body').css('overflow', 'hidden');
-    var wH = $(window).height();
-    var mH = $('.modal-wrapper').innerHeight();
+    var wH = $(window).height() * 0.948;
+    var mH = $('#modal-wrapper_pr').innerHeight();
     if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('iPod') > 0) {
       if (mH > wH) {
         bg.addEventListener('touchmove', prevent, {
@@ -104,8 +104,8 @@ $(function () {
   $('#fp-btn').click(function () {
     $('#modal-fp').fadeIn();
     $('body').css('overflow', 'hidden');
-    var wH = $(window).height();
-    var mH = $('.modal-wrapper').innerHeight();
+    var wH = $(window).height() * 0.948;
+    var mH = $('#modal-wrapper_fp').innerHeight();
     if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('iPod') > 0) {
       if (mH > wH) {
         bg.addEventListener('touchmove', prevent, {
@@ -136,8 +136,8 @@ $(function () {
   $('#wm-btn').click(function () {
     $('#modal-wm').fadeIn();
     $('body').css('overflow', 'hidden');
-    var wH = $(window).height();
-    var mH = $('.modal-wrapper').innerHeight();
+    var wH = $(window).height() * 0.948;
+    var mH = $('modal-wrapper_wm').innerHeight();
     if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('iPod') > 0) {
       if (mH > wH) {
         bg.addEventListener('touchmove', prevent, {
@@ -168,8 +168,8 @@ $(function () {
   $('#ga-btn').click(function () {
     $('#modal-ga').fadeIn();
     $('body').css('overflow', 'hidden');
-    var wH = $(window).height();
-    var mH = $('.modal-wrapper').innerHeight();
+    var wH = $(window).height() * 0.948;
+    var mH = $('#modal-wrapper_ga').innerHeight();
     if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('iPod') > 0) {
       if (mH > wH) {
         bg.addEventListener('touchmove', prevent, {
